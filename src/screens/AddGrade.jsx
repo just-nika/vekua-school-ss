db43906e2a8a55a6e05cd79dfa89f0efdf308224
just-nka <lobzhanidzenikolozi@gmail.com>
2022-07-10 14:13:33 +0400

    Grading function, grade/grades check, appeal add/chack and bug fixes

--- src/screens/AddGrade.jsx
+++ src/screens/AddGrade.jsx
@@ -88,6 +88,53 @@ function AddGrade() {
                 });
             }
         }
+        const AppealGrade = async () => {
+            const ClassNumber = document.getElementById("grade").value;
+            const code = document.getElementById("code").value;
+            if (ClassNumber == 7) {
+                const m = document.getElementById("MathAppeal").value;
+                await firestore.collection(ClassNumber).where("code", "==", code).get().then(async () => {
+                    const data = await firestore.collection(ClassNumber).get();
+                    setPosts(data.docs.map(doc => ({
+                        ...doc.data(),
+                        id: doc.id
+                    })))
+                    posts.map(async (post, index) => {
+                        if (post.code == code) {
+                            await firestore.collection(ClassNumber).doc(post.id).update({
+                                ap_m: m,
+                            }).then(() => {
+                                document.getElementById("code").value = "";
+                                document.getElementById("MathAppeal").value = "";
+                            })
+                        }
+                    })
+                });
+            }
+            else if (ClassNumber == 8 || ClassNumber == 9 || ClassNumber == 10 || ClassNumber == 11) {
+                const p = document.getElementById("PhysicsAppeal").value;
+                const m = document.getElementById("MathAppeal").value;
+                await firestore.collection(ClassNumber).where("code", "==", code).get().then(async () => {
+                    const data = await firestore.collection(ClassNumber).get();
+                    setPosts(data.docs.map(doc => ({
+                        ...doc.data(),
+                        id: doc.id
+                    })))
+                    posts.map(async (post, index) => {
+                        if (post.code == code) {
+                            await firestore.collection(ClassNumber).doc(post.id).update({
+                                ap_p: p,
+                                ap_m: m,
+                            }).then(() => {
+                                document.getElementById("code").value = "";
+                                document.getElementById("PhysicsAppeal").value = "";
+                                document.getElementById("MathAppeal").value = "";
+                            })
+                        }
+                    })
+                });
+            }
+        }
         return <>
             <Data/>
             <br />
@@ -105,10 +152,18 @@ function AddGrade() {
                 <p>არასაშაბათოების ქულები</p>
                 <input type="text" id='classNumber' placeholder='კლასი' /> <br /><br />
                 <input type="number" id='uniqueNumber' placeholder='უნიკალური ნომერი' /> <br /><br />
-                {/* <input type="text" id='IdNumber' placeholder='პირადი ნომერი' /> <br /><br /> */}
                 <input type="number" id='math' placeholder='მათემატიკის ქულა' /> <br /><br />
                 <input type="number" id='physics' placeholder='ფიზიკა ქულა' /> <br /><br />
                 <input type="button" onClick={OthersGrade} value="ქულის მინიჭება" />
+            </form>
+            <hr />
+            <form action="">
+                <p>აპელაციის ქულის შეტანა</p>
+                <input type="text" id='grade' placeholder='კლასი' /> <br /><br />
+                <input type="number" id='code' placeholder='უნიკალური ნომერი' /> <br /><br />
+                <input type="number" id='MathAppeal' placeholder='მათემატიკის აპელაციის ქულა' /> <br /><br />
+                <input type="number" id='PhysicsAppeal' placeholder='ფიზიკის აპელაციის ქულა' /> <br /><br />
+                <input type="button" onClick={AppealGrade} value="ქულის მინიჭება" />
             </form>
         </>
     }else {
